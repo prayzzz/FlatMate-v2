810b54f677e2f4705231031864db27c1fb644ff1
prayzzz <prayzzz@outlook.com>
2017-05-21 11:19:07 +0200

    - added data scripts

---
+++ data/00_DeleteData.sql
@@ -0,0 +1,11 @@
+----------
+-- List --
+DELETE FROM [List].[Item]
+DELETE FROM [List].[ItemGroup]
+DELETE FROM [List].[ItemList]
+GO
+
+-------------
+-- Account --
+DELETE FROM [Account].[User]
+GO
