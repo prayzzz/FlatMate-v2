94ee5fd4728a89a21a939ca6a662ff2af40b2b35
prayzzz <prayzzz@outlook.com>
2017-06-23 10:56:39 +0200

    - update webpack - added minification to frontent build

--- src/FlatMate.Web/wwwroot/js/app.ts
+++ src/FlatMate.Web/wwwroot/js/app.ts
@@ -1,7 +1,10 @@
 ﻿import * as ko from "knockout";
+import * as promiseFill from "es6-promise";
 import { KoStartup } from "./koStartup";
 import "./shared/objectExtensions";
 import { IWindow } from "./window";
+
+promiseFill.polyfill();
 
 // add knockout to window
 declare var window: IWindow;
--- src/FlatMate.Web/wwwroot/webpack.config.js
+++ src/FlatMate.Web/wwwroot/webpack.config.js
@@ -1,52 +1,77 @@
 ﻿const glob = require("glob");
-const path = require('path')
+const path = require('path');
 const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = [
-    {
-        name: "js",
-        entry: {
-            app: "./js/app.ts",
-            vendor: ["knockout", "knockout-dragdrop"]
-        },
-        output: {
-            path: path.resolve(__dirname, './dist'),
-            publicPath: '/dist/',
-            filename: '[name].js'
-        },
-        devtool: 'source-map',
-        resolve: {
-            extensions: [".ts", ".js"]
-        },
-        module: {
-            loaders: [
-                { test: /\.ts$/, loader: "ts-loader" }
-            ]
-        },
-        plugins: [
-            new webpack.optimize.CommonsChunkPlugin({ name: "vendor" })
-        ]
-    },
-    {
-        name: "css",
-        entry: glob.sync("./css/**/*.scss"),
-        output: {
-            filename: "./dist/app.css"
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.scss$/,
-                    use: ExtractTextPlugin.extract({
-                        fallback: "style-loader",
-                        use: "css-loader!sass-loader"
-                    })
-                }
-            ]
-        },
-        plugins: [
-            new ExtractTextPlugin("./dist/app.css")
-        ]
+module.exports = (env) => {
+    if (!env) {
+        env = "Debug";
     }
-];
+
+    console.log();
+    console.log(`Running webpack in ${env} mode`);
+    console.log();
+
+    /**
+     * Setup Javascript configuration
+     */
+    var jsConfig = {};
+    jsConfig.name = "js";
+    jsConfig.entry = {
+        app: "./js/app.ts",
+        vendor: ["knockout", "knockout-dragdrop", "es6-promise"]
+    };
+    jsConfig.output = {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: '[name].js'
+    };
+    jsConfig.resolve = { extensions: [".ts", ".js"] };
+    jsConfig.module = {
+        loaders: [{ test: /\.ts$/, loader: "ts-loader" }]
+    };
+    jsConfig.plugins = [
+        new webpack.optimize.CommonsChunkPlugin({ name: "vendor" })
+    ];
+
+    if ("Debug" === env) {
+        jsConfig.devtool = 'source-map';
+    }
+
+    if ("Release" === env) {
+        jsConfig.plugins.push(new webpack.optimize.UglifyJsPlugin());
+    }
+
+    /**
+     * Setup CSS configuration
+     */
+
+    var cssLoaderOptions = { minimize: false };
+    if ("Release" === env) {
+        cssLoaderOptions.minimize = true;
+    }
+
+    var cssConfig = {};
+    cssConfig.name = "css";
+    cssConfig.entry = glob.sync("./css/**/*.scss");
+    cssConfig.output = { filename: "./dist/app.css" };
+    cssConfig.module = {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [
+                        { loader: "css-loader", options: cssLoaderOptions },
+                        { loader: "sass-loader" }
+                    ]
+                })
+            }
+        ]
+    };
+    cssConfig.plugins = [
+        new ExtractTextPlugin("./dist/app.css")
+    ];
+
+    return [jsConfig, cssConfig];
+};
