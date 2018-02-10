a30b1cb2f522823c5e881146c8ae7f4a0551af1c
prayzzz <prayzzz@outlook.com>
2018-02-10 23:15:24 +0100

    - cache area restriction - styling

--- src/FlatMate.Module.Account/DataAccess/Users/UserRestrictedAreaRepository.cs
+++ src/FlatMate.Module.Account/DataAccess/Users/UserRestrictedAreaRepository.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using prayzzz.Common.Attributes;
 
 namespace FlatMate.Module.Account.DataAccess.Users
@@ -15,15 +15,24 @@ namespace FlatMate.Module.Account.DataAccess.Users
     public class UserRestrictedAreaRepository : IUserRestrictedAreaRepository
     {
         private readonly AccountDbContext _dbContext;
+        private readonly IMemoryCache _cache;
+        private readonly MemoryCacheEntryOptions _entryOptions;
 
-        public UserRestrictedAreaRepository(AccountDbContext dbContext)
+        public UserRestrictedAreaRepository(AccountDbContext dbContext, IMemoryCache cache)
         {
             _dbContext = dbContext;
+            _cache = cache;
+
+            _entryOptions = new MemoryCacheEntryOptions { AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1) };
         }
 
         public List<string> GetRestrictedAreas(int userId)
         {
-            return _dbContext.UserRestrictedAreas.Where(t => t.UserId == userId).Select(a => a.Area).ToList();
+            return _cache.GetOrCreate(userId, entry =>
+            {
+                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1);
+                return _dbContext.UserRestrictedAreas.Where(t => t.UserId == userId).Select(a => a.Area).ToList();
+            });
         }
     }
 }
